9e66f4dcd8bb3a7db38ed9e65660abd50cd5ca68
Laura Martins <lauraalvesmartins04@gmail.com>
2022-09-14 16:08:20 -0300

    .

--- src/components/context/index.jsx
+++ src/components/context/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, createContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { api, createSession} from '../../service/api';
 //Auths :)
 
 export const AuthContext = createContext();
@@ -21,29 +21,34 @@ export const AuthProvicer = ({children}) => {
         setLoading(false)
     }, [])
 
-    const login = (email, password) => {
+    const login = async (email, password) => {
         console.log('login auth', {email, password})
         
-        const LoggedUser = {
-            id: '123',
-            email,
-        }
+        const response = await createSession(email, password)
+
+        console.log('login', response.data)
+
+        const LoggedUser = response.data.user
+        const token = response.data.token;
     
         localStorage.setItem("user", JSON.stringify(LoggedUser))
+        localStorage.setItem("token", token)
 
-        if(password === 'secret'){
-            setUser(LoggedUser)
-            navigate('/')
-        }
+        api.defaults.headers.Autorization = `Bearer ${token}`
+
+        setUser(LoggedUser)
+        navigate('/')
     }
     const logout = () => {
         console.log("logout")
         localStorage.removeItem('user')
+        localStorage.removeItem('token')
+        api.defaults.headers.Autorization = null
         setUser(null)
         navigate('login')
     }
     
-    return(
+    return (
         <AuthContext.Provider value={{authenticated: !!user, user,loading, login, logout}}>
             {children}
         </AuthContext.Provider>//Provider => Prover
--- src/service/api.js
+++ src/service/api.js
@@ -4,6 +4,10 @@ export const api = axios.create({
     baseURL: 'http://localhost:3000'
 })
 
-export const createSession = async (email, password) =>{
+export const createSession = async (email, password) => {
     return api.post('/sessions', {email, password})
+}
+
+export const getUsers = async() => {
+    return api.get('/users')
 }
